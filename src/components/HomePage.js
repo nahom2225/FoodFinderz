6f069f84cd4379066c71421f0e08b0904abed09f
nahom2225 <nahomd10@gmail.com>
2024-06-15 16:17:17 -0700

    readding credential sending

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -24,6 +24,7 @@ export default function Home(props) {
     useEffect(() => {
         // code to run on component mount
         fetch(`${backendUrl}/api/account-in-session`, {
+            credentials: 'include',  
             headers: new Headers({
               "ngrok-skip-browser-warning": "6024",
             }),
