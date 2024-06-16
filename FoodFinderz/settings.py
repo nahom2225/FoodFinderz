31f74212d308d32b8cfaa189e9fd4d6b9efc42b1
nahom2225 <nahomd10@gmail.com>
2024-06-15 18:13:34 -0700

    adding redis

--- FoodFinderz/settings.py
+++ FoodFinderz/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': 'redis://localhost:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
