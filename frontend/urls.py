f43704ac01e1460d72c8ab600f2e8105e860e88c
nahom2225 <nahomd10@gmail.com>
2024-06-17 22:45:11 -0700

    another fix attempt

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index, name = 'home'),
+    path('', index, name = 'index'),
     path('create-account', index),
     path('frontpage', index),
     path('login', index),
